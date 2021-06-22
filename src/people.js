1b61441f5825a057a097950b0c0372b0badcc41b
SorraimiRivas <sorraimi.rivas@hotmail.com>
2021-06-22 09:26:02 -0400

    Added endpoints

---
+++ src/people.js
@@ -0,0 +1,14 @@
+export let people = [{
+
+    name:"John",
+    age:"45",
+    hairColor:"blonde",
+},{
+    name:"Gearless",
+    age:"30",
+    hairColor:"brown"
+},{
+    name:"Janna",
+    age:"1000",
+    hairColor:"red",
+}];
